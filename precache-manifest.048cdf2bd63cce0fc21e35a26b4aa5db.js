b9442bd6b6826f01a94ed8e59fa5708fe3dc2790
Martin Rodalgaard <mrodalgaard@gmail.com>
2019-11-23 21:54:52 +0100

    Updates

---
+++ precache-manifest.048cdf2bd63cce0fc21e35a26b4aa5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9600bf27c385c25a6530fbe2c7cf94d",
+    "url": "/bare-lige-ometer/index.html"
+  },
+  {
+    "revision": "6a90ff672936ee037228",
+    "url": "/bare-lige-ometer/static/css/2.156fe21f.chunk.css"
+  },
+  {
+    "revision": "834f28f0286eedfdbc0c",
+    "url": "/bare-lige-ometer/static/css/main.474aa5c9.chunk.css"
+  },
+  {
+    "revision": "6a90ff672936ee037228",
+    "url": "/bare-lige-ometer/static/js/2.260ea6c3.chunk.js"
+  },
+  {
+    "revision": "834f28f0286eedfdbc0c",
+    "url": "/bare-lige-ometer/static/js/main.c8862b82.chunk.js"
+  },
+  {
+    "revision": "436e8ee7908ef50de7ea",
+    "url": "/bare-lige-ometer/static/js/runtime-main.73b6edc5.js"
+  }
+]);
