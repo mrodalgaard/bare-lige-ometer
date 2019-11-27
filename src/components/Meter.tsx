10cf8ba427f41b582e717e3b0b89e436981b0926
Martin Rodalgaard <mrodalgaard@gmail.com>
2019-11-27 23:18:53 +0100

    Set query parameter

--- src/components/Meter.tsx
+++ src/components/Meter.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GaugeChart from "react-gauge-chart";
+import { useQueryParam, NumberParam } from "use-query-params";
 
 import { meterColors, getMeterColor } from "../util/constants";
 
 const Meter = () => {
   const [gaugeValue, setGaugeValue] = useState(0);
+  const [paramValue, setParamValue] = useQueryParam("value", NumberParam);
+
+  useEffect(() => {
+    if (paramValue !== undefined) {
+      setGaugeValue(paramValue);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setGaugeValue]);
+
+  useEffect(() => {
+    setParamValue(gaugeValue);
+  }, [setParamValue, gaugeValue]);
 
   const updateGauge = (value: number) => {
     const capValue = Math.min(100, Math.max(0, value * 100));
