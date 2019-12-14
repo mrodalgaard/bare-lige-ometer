64e509466b0dbd42fa560713129e48ae1206b062
Martin Rodalgaard <mrodalgaard@gmail.com>
2019-12-14 22:03:46 +0100

    Fix test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// react-gauge does not work in tests
-jest.mock("react-gauge-chart", () => {
-  return function Meter() {
-    return <div>GAUGE</div>;
-  };
-});
-
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
