c94253ff4fd991e0dd16e494f86b58f34f402a49
Martin Rodalgaard <mrodalgaard@gmail.com>
2020-11-17 21:13:28 +0100

    Update test

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,9 +1,14 @@
+import Meter from "components/Meter";
+import Title from "components/Title";
 import React from "react";
-import ReactDOM from "react-dom";
+import TestRenderer from "react-test-renderer";
 import App from "../App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // @testing-library/react does not work with the canvas used in Meter
+  const testRenderer = TestRenderer.create(<App />);
+  const testInstance = testRenderer.root;
+
+  expect(testInstance.findByType(Title)).toBeDefined();
+  expect(testInstance.findByType(Meter)).toBeDefined();
 });
