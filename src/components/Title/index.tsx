c3113244908dd0ce01203f4d7f2653acd2d2ea4f
Martin Rodalgaard <mrodalgaard@gmail.com>
2024-10-20 20:22:37 +0200

    Color title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,17 +1,40 @@
+import { AppContext } from 'contexts/AppContext';
+import { useContext } from 'react';
 import styled from 'styled-components';
-import { APP_TITLE } from 'util/constants';
+import { APP_TITLE, METER_COLORS } from 'util/constants';
+import { theme } from 'util/theme';
 
 const Header = styled.h1`
   font-size: 80px;
-  color: ${({ theme }) => theme.colors.header};
   text-align: center;
   margin-bottom: 10px;
 `;
 
+const HeaderLink = styled.a`
+  text-decoration: none;
+  color: ${({ color }) => color};
+`;
+
+const getMeterColor = (percentage: number | undefined | null): string => {
+  if (percentage === undefined || percentage === null) {
+    return theme.colors.header;
+  }
+  const index = Math.floor(METER_COLORS.length * (percentage / 100));
+  return METER_COLORS[Math.min(index, METER_COLORS.length - 1)];
+};
+
 export const Title = () => {
+  const { value } = useContext(AppContext);
+
+  const color = getMeterColor(value);
+
   return (
     <header>
-      <Header>{APP_TITLE}</Header>
+      <Header>
+        <HeaderLink color={color} href="/">
+          {APP_TITLE}
+        </HeaderLink>
+      </Header>
     </header>
   );
 };
